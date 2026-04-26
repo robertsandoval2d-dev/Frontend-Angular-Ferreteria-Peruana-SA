d2337f11a063708e093aa24524f068e7ad2456e4
Alonso Sandoval <robertsandoval2d@gmail.com>
2026-04-25 23:28:27 -0500

    primer commit, estructura del proyecto frontend

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  constructor(private router: Router) {}
+
+  irALogin() {
+    this.router.navigate(['/login']);
+  }
+}
