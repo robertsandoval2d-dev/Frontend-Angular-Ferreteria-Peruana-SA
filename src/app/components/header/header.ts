883b117fdbc6357c2366cbd33d426613d3f22a23
Alonso Sandoval <robertsandoval2d@gmail.com>
2026-04-26 20:19:56 -0500

    agregado conexion del homepage con el login

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router ,RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
