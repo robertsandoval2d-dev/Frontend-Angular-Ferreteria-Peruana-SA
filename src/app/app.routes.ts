4ca93e9d7c90bbffb296fca83bea465ca49f03a8
SebastianHuaringa <rodrigocastillo3805@gmail.com>
2026-05-06 20:37:38 -0500

    Sidebar v1: Falta corregir estructura y posición en la dir privadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,26 +8,33 @@ import { Login} from './pages/public/login/login';
 //Import de las paginas privadas (Con login)
 import { Dashboard} from './pages/private/dashboard/dashboard';
 import { Clientes } from './pages/private/clientes/clientes';
+import { Sidebar } from './components/sidebar/sidebar';
 
 
 //Se daclaran en este route las zonas layout pública y privada
 export const routes: Routes = [
-    //ZONA PÚBLICA
-    {
-        path:'',
-        component:PublicLayout,
-        children:[
-            { path: '', component: Home },
-            { path: 'Login', component: Login}
-        ]
-    },
-    //ZONA PRIVADA
+    //PRUEBAS
     {
         path: '',
-        component: PrivateLayout,
-        children:[
-            { path: 'dashboard', component: Dashboard },
-            { path: 'clientes', component: Clientes } 
-        ]
+        component: Sidebar
     }
+    
+    // //ZONA PÚBLICA
+    // {
+    //     path:'',
+    //     component:PublicLayout,
+    //     children:[
+    //         { path: '', component: Home },
+    //         { path: 'Login', component: Login}
+    //     ]
+    // },
+    // //ZONA PRIVADA
+    // {
+    //     path: '',
+    //     component: PrivateLayout,
+    //     children:[
+    //         { path: 'dashboard', component: Dashboard },
+    //         { path: 'clientes', component: Clientes } 
+    //     ]
+    // }
 ];
