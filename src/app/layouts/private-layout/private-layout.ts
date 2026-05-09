de6776205e8fb357afbca25a287a7c5476afc914
Rodrigo Quispe <rodrigo.quispeg@unmsm.edu.pe>
2026-05-09 12:25:24 -0500

    Estructura terminada

--- src/app/layouts/private-layout/private-layout.ts
+++ src/app/layouts/private-layout/private-layout.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from '../../components/header/header';
+import { Footer } from '../../components/footer/footer';
+import { Sidebar } from '../../components/sidebar/sidebar';
 
 @Component({
   selector: 'app-private-layout',
-  imports: [],
+  imports: [RouterOutlet, Header, Footer, Sidebar],
   templateUrl: './private-layout.html',
   styleUrl: './private-layout.scss',
 })
