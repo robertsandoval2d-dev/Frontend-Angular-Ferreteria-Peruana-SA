56c16d1622c13b44941715a25f3506a9e4b5d3f5
Rodrigo UNMSM <rodrigo.quispeg@unmsm.edu.pe>
2026-06-17 11:15:11 -0500

    feat(mensajeria v1.1): Implementación del diseño para el GET /logistica/mensajeria/mensajes

--- src/app/pages/private/mensajeria/components/tabla-mensajes/tabla-mensajes.ts
+++ src/app/pages/private/mensajeria/components/tabla-mensajes/tabla-mensajes.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
+
+import { MensajeListResponse } from '../../models/response/mensaje-list-response';
+import { MensajeriaService }  from '../../services/mensajeria-service';
 
 @Component({
   selector: 'app-tabla-mensajes',
@@ -6,4 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './tabla-mensajes.html',
   styleUrl: './tabla-mensajes.scss',
 })
-export class TablaMensajes {}
+export class TablaMensajes implements OnInit{
+
+  private cdr = inject(ChangeDetectorRef);
+  
+  mensajeriaService = inject(MensajeriaService);
+  listaMensajes : MensajeListResponse[] = [];
+
+  ngOnInit() {
+    this.cargarMensajes();
+    this.cdr.detectChanges();
+    
+  }
+
+
+
+  cargarMensajes(){
+    this.mensajeriaService.listarMensajes().subscribe({
+      next:(respuestaBackend) =>{
+        this.listaMensajes = respuestaBackend
+        console.log("Llegó las lista de mensajes: ", respuestaBackend);
+      },
+      error:(errorBackend) => {
+        console.log("Error en el envío: ", errorBackend);
+      }
+    })
+  }
+}
---
+++ src/app/pages/private/mensajeria/services/mensajeria-service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
+
+import { MensajeListResponse } from '../models/response/mensaje-list-response';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MensajeriaService {
+
+  private http = inject(HttpClient);
+
+  //GET
+  listarMensajes(): Observable<MensajeListResponse[]> {
+    return this.http.get<MensajeListResponse[]>(`${environment.url}/trabajadores/sucursales`);
+  }
+
+}
